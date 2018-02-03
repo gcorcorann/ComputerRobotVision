fdb13a2e9eb39906c7da3feb91be79190064801c
Gary Corcoran <gary.corcoran@mail.mcgill.ca>
2018-02-03 15:32:12 -0500

    Added LeNet5 model.

--- src/model.py
+++ src/model.py
@@ -5,6 +5,30 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 import torchvision.models as models
 
+class LeNet5(nn.Module):
+    """
+    LeNet5.
+    """
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool1 = nn.MaxPool2d(2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.pool2 = nn.MaxPool2d(2)
+        self.fc1 = nn.Linear(16*5*5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, inp):
+        out = self.pool1(F.relu(self.conv1(inp)))
+        out = self.pool2(F.relu(self.conv2(out)))
+        # reshape into [batchSize x numFeats]
+        out = out.view(out.size(0), -1)
+        out = F.relu(self.fc1(out))
+        out = F.relu(self.fc2(out))
+        out = self.fc3(out)
+        return out
+
 class Network1(nn.Module):
     """
     LeNet5 + LSTM.
