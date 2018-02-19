f87fbb6e9f13098355ca73e77d4a80bee8533441
Gary Corcoran <gary.corcoran@mail.mcgill.ca>
2018-02-19 12:16:07 -0500

    Updated get_loaders function.

--- src/dataloader.py
+++ src/dataloader.py
@@ -4,11 +4,94 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 import numbers
+import math
 import torch
 from torch.utils.data import Dataset, DataLoader
 from torch.utils.data.sampler import SubsetRandomSampler
 import torchvision
 from torchvision import transforms
+
+class FlowDataset(Dataset):
+    """
+    Flow Attention Dataset.
+    """
+    def __init__(self, labels_path, transform=None):
+        """
+        @param  labels_path:    path to text file with annotations
+                                    @pre: string
+        @param  transform:      optional transform to be applied on image
+                                    @pre: callable
+        """
+        # read video path and labels
+        with open(labels_path, 'r') as f:
+            data = f.read()
+            data = data.split()
+            data = np.array(data)
+            data = data.reshape(-1,2)
+
+        np.random.shuffle(data)
+        self.data = data
+        self.transform = transform
+        self.cap = cv2.VideoCapture()
+
+    def __len__(self):
+        """
+        Retrieve Dataset Length.
+        """
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        """
+        Retrieve Next Item in Dataset.
+
+        @return sample: sample['X'] contains input data while sample['y']
+                        contains attention label.
+        """
+        # list to store frames
+        X = []
+        y = int(self.data[idx, 1]) - 1
+        video_path = self.data[idx, 0]
+        # open video
+        self.cap.open(video_path)
+        # read initial frame
+        _, frame1 = self.cap.read()
+        # apply transform
+        if self.transform:
+            frame1 = self.transform(frame1)
+
+        # convert to grayscale
+        frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+        # for each remaining frame in video
+        for i in range(99):
+            # read second frame
+            _, frame2 = self.cap.read()
+            # sample video 5 frames apart
+            if i != 0 and i % 5 == 0:
+                # apply transform
+                if self.transform:
+                    frame2 = self.transform(frame2)
+
+                # convert to grayscale
+                frame2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+                # compute flow
+                flow = cv2.calcOpticalFlowFarneback(frame1, frame2, None,
+                        0.5, 3, 15, 3, 5, 1.2, 0)
+                # normalize between 0-1
+                cv2.normalize(flow, flow, 0, 1, cv2.NORM_MINMAX)
+                # store flow in list
+                X.append(flow)
+                # set initial frame to second frame
+                frame1 = frame2
+
+        # store in ndarray
+        X = np.array(X, dtype=np.float32)
+        # reformat to [numSeqs x numChannels x Height x Width]
+        X = np.transpose(X, (0,3,1,2))
+        # store in sample
+        sample = {'X': X, 'y': y}
+        # release video capture device
+        self.cap.release()
+        return sample
 
 class AttentionDataset(Dataset):
     """
@@ -48,7 +131,7 @@ class AttentionDataset(Dataset):
             if not ret:
                 break
             # sample video 5 frames apart
-            if i % 20 == 0:
+            if i % 10 == 0:
                 # store frame
                 X.append(frame)
 
@@ -258,14 +341,18 @@ class Normalize():
         video = np.transpose(video, (0,3,1,2))
         return video
 
+def get_loaders(labels_path, batch_size, num_workers, gpu):
+    """Return dictionary of torch.utils.data.DataLoader.
 
-def main():
-    """Main Function."""
-    import time
+    Args:
+        labels_path (string): path to text file with annotations
+        batch_size (int): number of instances in batch
+        num_workers (int): number of subprocesses used for data loading
+        gpu (bool): presence of gpu
 
-    labels_path = '/home/gary/datasets/accv/labels_gary.txt'
-    batch_size = 5
-
+    Returns:
+        torch.utils.data.DataLoader: dataloader for custom dataset
+    """
     # data augmentation and normalization for training
     # just normalization for validation
     data_transforms = {
@@ -284,9 +371,39 @@ def main():
             }
 
     # create dataset object
-    dataset = AttentionDataset(labels_path, data_transforms['Train'])
-    dataloader = DataLoader(dataset, batch_size, 
-            shuffle=False, num_workers=4)
+    datasets = {x: AttentionDataset(labels_path, data_transforms[x])
+            for x in ['Train', 'Valid']}
+
+    # random split for training and validation
+    num_instances = len(datasets['Train'])
+    indices = list(range(num_instances))
+    split = math.floor(num_instances * 0.8)
+    train_indices, valid_indices = indices[:split], indices[split]
+    samplers = {'Train': SubsetRandomSampler(indices[:split]),
+                'Valid': SubsetRandomSampler(indices[split:])}
+
+    # create dataloders
+    dataloaders = {
+            x: DataLoader(datasets[x],
+                batch_size=batch_size, sampler=samplers[x],
+                num_workers=num_workers, pin_memory=gpu)
+            for x in ['Train', 'Valid']
+            }
+
+    return dataloaders
+
+def main():
+    """Main Function."""
+    import time
+
+    # hyperparameters
+    labels_path = '/home/gary/datasets/accv/labels_med.txt'
+    batch_size = 5
+    num_workers = 8
+    gpu = torch.cuda.is_available()
+
+    # dictionary of dataloaders
+    dataloaders = get_loaders(labels_path, batch_size, num_workers, gpu)
 
     def imshow(inp, title=None):
         """Imshow for Tensor."""
@@ -301,12 +418,12 @@ def main():
         if title is not None:
             plt.title(title)
 
-    # get a bunch of training data
-    sampled_batch = next(iter(dataloader))
-    data, labels = sampled_batch['X'], sampled_batch['y']
+    # retrieve a batch of training data
+    train_batch = next(iter(dataloaders['Valid']))
+    data, labels = train_batch['X'], train_batch['y']
 
-    classes = ['Low Attention', 'Medium Attention', 'High Attention', 
-    'Very High Attention']
+    classes = ['Low Attention', 'Medium Attention', 
+            'High Attention', 'Very High Attention']
     print(classes)
 
     plt.figure()
@@ -314,174 +431,9 @@ def main():
         out = torchvision.utils.make_grid(data[i], nrow=10)
         plt.subplot(batch_size, 1, i+1), imshow(out, classes[labels[i]])
         plt.xticks([]), plt.yticks([])
+
+    plt.tight_layout()
     plt.show()
-
-
-#class FlowDataset(Dataset):
-#    """
-#    Flow Attention Dataset.
-#    """
-#    def __init__(self, labels_path, transform=None):
-#        """
-#        @param  labels_path:    path to text file with annotations
-#                                    @pre: string
-#        @param  transform:      optional transform to be applied on image
-#                                    @pre: callable
-#        """
-#        # read video path and labels
-#        with open(labels_path, 'r') as f:
-#            data = f.read()
-#            data = data.split()
-#            data = np.array(data)
-#            data = data.reshape(-1,2)
-#
-#        np.random.shuffle(data)
-#        self.data = data
-#        self.transform = transform
-#        self.cap = cv2.VideoCapture()
-#
-#    def __len__(self):
-#        """
-#        Retrieve Dataset Length.
-#        """
-#        return len(self.data)
-#
-#    def __getitem__(self, idx):
-#        """
-#        Retrieve Next Item in Dataset.
-#
-#        @return sample: sample['X'] contains input data while sample['y']
-#                        contains attention label.
-#        """
-#        # list to store frames
-#        X = []
-#        y = int(self.data[idx, 1]) - 1
-#        video_path = self.data[idx, 0]
-#        # open video
-#        self.cap.open(video_path)
-#        # read initial frame
-#        _, frame1 = self.cap.read()
-#        # apply transform
-#        if self.transform:
-#            frame1 = self.transform(frame1)
-#
-#        # convert to grayscale
-#        frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
-#        # for each remaining frame in video
-#        for i in range(99):
-#            # read second frame
-#            _, frame2 = self.cap.read()
-#            # sample video 5 frames apart
-#            if i != 0 and i % 5 == 0:
-#                # apply transform
-#                if self.transform:
-#                    frame2 = self.transform(frame2)
-#
-#                # convert to grayscale
-#                frame2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
-#                # compute flow
-#                flow = cv2.calcOpticalFlowFarneback(frame1, frame2, None,
-#                        0.5, 3, 15, 3, 5, 1.2, 0)
-#                # normalize between 0-1
-#                cv2.normalize(flow, flow, 0, 1, cv2.NORM_MINMAX)
-#                # store flow in list
-#                X.append(flow)
-#                # set initial frame to second frame
-#                frame1 = frame2
-#
-#        # store in ndarray
-#        X = np.array(X, dtype=np.float32)
-#        # reformat to [numSeqs x numChannels x Height x Width]
-#        X = np.transpose(X, (0,3,1,2))
-#        # store in sample
-#        sample = {'X': X, 'y': y}
-#        # release video capture device
-#        self.cap.release()
-#        return sample
-#
-#
-#def get_loaders(labels_path, input_size, batch_size, num_workers):
-#    """
-#    Get DataLoaders.
-#
-#    @param  labels_path:    path to text file with annotations
-#                                @pre: string
-#    @param  input_size:     resize frame to this size
-#                                @pre: tuple of integers
-#
-#    @return torch.utils.data.DataLoader for custom dataset
-#    """
-#    # data transforms
-#    composed = transforms.Compose([
-#        Resize(input_size),
-#        Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-#        ])
-#    # create dataset
-#    dataset = AttentionDataset(labels_path, transform=composed)
-##    dataset = FlowDataset(labels_path, transform=Resize(input_size)) 
-#
-#    # split dataset into training and validation
-#    num_instances = len(dataset)
-#    indices = list(range(num_instances))
-#    split = int(np.floor(num_instances * 0.8))
-#    train_idx, valid_idx = indices[:split][:50], indices[split:][:1]
-#    train_sampler = SubsetRandomSampler(train_idx)
-#    valid_sampler = SubsetRandomSampler(valid_idx)
-#
-#    # check if GPU is available
-#    gpu = torch.cuda.is_available()
-#    # dataloaders
-#    dataloaders = {
-#            'Train': DataLoader(dataset,
-#                batch_size=batch_size,
-#                sampler=train_sampler,
-#                num_workers=num_workers,
-#                pin_memory=gpu),
-#            'Valid': DataLoader(dataset,
-#                batch_size=batch_size,
-#                sampler=valid_sampler,
-#                num_workers=num_workers,
-#                pin_memory=gpu)
-#            }
-#
-#    return dataloaders
-#
-#def main():
-#    """Main Function."""
-#    import time
-#
-#    # start timer
-#    start = time.time()
-#
-#    labels_path = '/home/gary/datasets/accv/labels_gary.txt'
-#    input_size = (224, 224)
-#    batch_size = 10
-#
-#
-#    dataset = AttentionDataset(labels_path)
-#
-#    for k in range(len(dataset)):
-#        sample = dataset[k]
-#        data, label = sample['X'], sample['y']
-#        print(data.shape, label)
-#
-#        if k == 4:
-#            break
-#
-##    # get DataLoaders
-##    dataloaders = get_loaders(labels_path, input_size, batch_size,
-##            num_workers=4)
-##
-##    print('Training Batches Size:', len(dataloaders['Train']))
-##    print('Validation Batches Size:', len(dataloaders['Valid']))
-##
-##    # go through validation set
-##    for i, sampled_batch in enumerate(dataloaders['Valid']):
-##        print(i, sampled_batch['X'].size(), sampled_batch['y'].size())
-##        if i == 3:
-##            break
-##
-##    print('Elapsed Time: {} seconds'.format(time.time()-start))
 
 if __name__ == '__main__':
     main()
