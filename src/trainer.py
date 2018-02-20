895ea79037b49f6b12ddda1002a65d02dcacc8b7
Gary Corcoran <gary.corcoran@mail.mcgill.ca>
2018-02-20 16:15:09 -0500

    Added training for hyper-parameter tuning.

--- src/trainer.py
+++ src/trainer.py
@@ -14,18 +14,26 @@ def train_network(network, dataloaders, dataset_sizes, criterion,
 
     Args:
         network (torchvision.models): network to train
+
         dataloaders (dictionary): contains torch.utils.data.DataLoader for both
-            training and validation
+                                    training and validation
         dataset_sizes (dictionary): size of training and validation datasets
+
         criterion (torch.nn.modules.loss): loss function
+
         opitimier (torch.optim): optimization algorithm.
+
         num_epochs (int): number of epochs used for training
+
         GPU (bool): gpu availability
 
     Returns:
         torchvision.models: best trained model
+
         float: best validaion accuracy
     """
+    # start timer
+    start = time.time()
     # store network to GPU
     if GPU:
         network = network.cuda()
@@ -35,8 +43,6 @@ def train_network(network, dataloaders, dataset_sizes, criterion,
     best_acc = 0.0
 
     for epoch in range(num_epochs):
-        # start timer
-        start = time.time()
         print()
         print('Epoch {}/{}'.format(epoch+1, num_epochs))
         print('-' * 10)
@@ -101,13 +107,12 @@ def train_network(network, dataloaders, dataset_sizes, criterion,
 
     # print elapsed time
     time_elapsed = time.time() - start
+    print()
     print('Training Complete in {:.0f}m {:.0f}s'.format(
     time_elapsed // 60, time_elapsed % 60))
 
     # load the best model weights
     network.load_state_dict(best_model_wts)
-    # save the best model weights to disk
-    torch.save(network.state_dict(), '../data/net_params.pkl')
     return network, best_acc
 
 def main():
@@ -119,11 +124,11 @@ def main():
     # network parameters
     seq_length = 20
     input_size = (224,224)
-    rnn_hidden = 128
-    rnn_layers = 1
+    rnn_hiddens = [32, 64]
+    rnn_layers = [1, 2]
     # training parameters
-    num_epochs = 5
-    learning_rate = 1e-4
+    num_epochs = 2
+    learning_rates = [1e-3]
     criterion = nn.CrossEntropyLoss()
 
     # create dataloaders object
@@ -132,16 +137,35 @@ def main():
     print('Training Dataset:', dataset_sizes['Train'])
     print('Validation Dataset:', dataset_sizes['Valid'])
 
-    # create network and optimizer
-    net = model.VGG(rnn_hidden, rnn_layers)
-    print(net)
-    params = list(net.lstm.parameters()) + list(net.fc.parameters())
-    optimizer = optim.Adam(params, learning_rate)
-
-    # train network
-    net, best_acc = train_network(net, dataloaders, dataset_sizes, 
-            criterion, optimizer, num_epochs, GPU)
-    print('Best Validation Accuracy:', best_acc)
+    best_acc = 0
+    best_params = {}
+    # train for all sets of hyper parameters
+    for lr in learning_rates:
+        for rnn_layer in rnn_layers:
+            for rnn_hidden in rnn_hiddens:
+                print()
+                print('Training Parameters:')
+                s = ('learning_rate: {}, rnn_hidden: {}, rnn_layer: {}')
+                print(s.format(lr, rnn_hidden, rnn_layer))
+                # create network and optimizer
+                net = model.VGG(rnn_hidden, rnn_layer)
+                params = list(net.lstm.parameters()) \
+                        + list(net.fc.parameters())
+                optimizer = optim.Adam(params, lr)
+                # train the network
+                net, val_acc = train_network(net, dataloaders,
+                        dataset_sizes, criterion, optimizer,
+                        num_epochs, GPU)
+                print('Best Validation Acc:', val_acc)
+                print('-' * 70)
+                if val_acc > best_acc:
+                    best_params = {'lr': lr, 'rnn_layer': rnn_layer,
+                            'rnn_hidden': rnn_hidden}
+                    torch.save(net.state_dict(), '../data/net_params.pkl')
+                    best_acc = val_acc
+                    
+    print()
+    print(best_params, 'accuracy:', best_acc)
 
 if __name__ == '__main__':
     main()
